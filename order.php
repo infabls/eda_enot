087f38a88cfc7f0fb209cf9c7415f3b182ee2157
infabls <infabls.com@gmail.com>
2020-12-22 17:52:49 +0600

    initial commit

---
+++ order.php
@@ -0,0 +1,15 @@
+<?php 
+if (isset($_POST['submit'])) {
+
+}
+
+var_dump($_POST['products']);
+$ass = json_decode($_POST['products']);
+// var_dump($ass);
+for ($i=0; $i < count($ass); $i++) {
+	echo "Ваш заказ <br>";
+	echo "Цена " .$ass[$i]->price . "<br>";
+	echo "<br>";
+}
+
+ ?>
