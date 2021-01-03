c4fb20bdc7cccc26c61541cd4e34e0e0be0b5953
bnvStorm <39797487+bnvStorm@users.noreply.github.com>
2021-01-04 01:10:49 +0600

    arrrgght

--- order.php
+++ order.php
@@ -37,7 +37,7 @@ if (!isset($_POST['submit'])) {
 	$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
 	if ($sendToTelegram) {
-	  header('Location: thank-you.html');
+	  header('Location: index.php');
 	} else {
 	  echo "Error";
 	  // при ошибке сохранять в локальный файл 
