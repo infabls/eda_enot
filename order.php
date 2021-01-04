3737f5a498dcce2ac9862f9ad85fb1022581c0da
infabls <infabls.com@gmail.com>
2021-01-04 23:17:32 +0600

    add billing type field to telegram sender function

--- order.php
+++ order.php
@@ -7,6 +7,14 @@ if (!isset($_POST['submit'])) {
 	$name = $_POST['user_name'];
 	$phone = $_POST['user_phone'];
 	$email = $_POST['user_email'];
+	$billing_type = $_POST['contact'];
+	if ($billing_type == 'card') {
+		$bill_type = 'Оплата картой';
+	} elseif ($billing_type == 'cash') {
+		$bill_type = 'Наличные';
+	} else {
+		$bill_type = 'Не указан';	
+	}
 	$token = "1415010068:AAGn5U8VZJNBzB_29-59l3nmAG4gUdZzdZE";
 	$chat_id = "424732045";
 	$ass = json_decode($_POST['products']);
@@ -25,6 +33,7 @@ if (!isset($_POST['submit'])) {
 	  'Телефон: ' => $phone,
 	  'Email: ' => $email,
 	  'Итоговая сумма: ' => $totalprice . ' тг',
+	  'Способ оплаты: ' => $bill_type,
 	  'Детали заказа: ' => $mssg,
 	);
 
@@ -35,17 +44,16 @@ if (!isset($_POST['submit'])) {
 
 	// отправка в телеграм
 	$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
-
 	if ($sendToTelegram) {
 	  header('Location: index.php');
 	} else {
-	  echo "Error";
+	  echo "К сожалению не получилось отправить вашу заявку. Вы можете сделать это по телефону";
 	  // при ошибке сохранять в локальный файл 
 	  foreach($arr as $key => $value) {
 	  $txthtml .= "<b>".$key."</b> ".$value;
 		};
-		var_dump($txthtml);
-		echo "<a href='inc/orders.html'>Заявки локально</a>";
+		// var_dump($txthtml);
+		// echo "<a href='inc/orders.html'>Заявки локально</a>";
 	  	file_put_contents('inc/orders.html', $txthtml . PHP_EOL, FILE_APPEND);
 	}
 	
