28faab89e16aae27e3a67d7577e92c431bd9926d
bnvStorm <39797487+bnvStorm@users.noreply.github.com>
2021-01-05 18:28:57 +0600

    data upd

--- inc/csvphp.php
+++ inc/csvphp.php
@@ -1,7 +1,7 @@
 <?php 
 // качаем категории и страницы из гугл диска и сохраняем на сервере
-// $categories = file_get_contents('http://docs.google.com/spreadsheets/d/1R87zOW2-990ATjZmc9qHu_CLqkFvLdqFJlY3TWzHgmU/export?format=csv&gid=1689854645');
-// file_put_contents('categories.csv', $categories);
+$categories = file_get_contents('http://docs.google.com/spreadsheets/d/1R87zOW2-990ATjZmc9qHu_CLqkFvLdqFJlY3TWzHgmU/export?format=csv&gid=1689854645');
+file_put_contents('categories.csv', $categories);
 
 function display_products($category,$array) {
 	require_once 'mobileDetect.php';
