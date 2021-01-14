08e625192173bc66e23b47ecd97ba03480a1b499
bnvStorm <39797487+bnvStorm@users.noreply.github.com>
2021-01-14 15:28:30 +0600

    content upd

--- inc/header.php
+++ inc/header.php
@@ -6,8 +6,8 @@
    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
 
-   <title>Кухонное хозяйство дядющки Сэма</title>
-   <meta name="description" content="Лидер на рынке вторичного сырья в Казахстане. Перерабатываем больше десятка опасных отходов. 8 лет на рынке Казахстана">
+   <title>Американская кухня от Дяди Сэма</title>
+   <meta name="description" content="Американская кухня от Дяди Сэма">
    <meta name="keywords" content="вторсырье, переработка, вторичное сырье">
    <meta name="robots" content="index,follow">
    <link rel="canonical" href="https://ddsam.kz">
@@ -51,22 +51,24 @@
    <link rel="stylesheet" href="/css/slick-theme.css">
    <link rel="stylesheet" href="/css/style.css">
    <link rel="stylesheet" href="/css/site.addons.css">
+   <link rel="stylesheet" href="/css/media.css">
    <script src="/js/jquery.min.js"></script>
    <script src="/js/lazy.js"></script>
 </head>
 <body>
    <div class="navbar">
       <div class="container">
-         <div class="tel-fax-mail-head">
-            <a target="_blank" rel="nofollow" href="https://wa.me/77769674545/"><img src="https://img.icons8.com/color/48/000000/whatsapp--v6.png"/></a>
-            <a href="tel:+77769674545">+7 776 967 45 45</a>
-         </div>
          <div class="site-title">
             <a href="/" class="logo">
                <img src="/img/logo.png" alt="logo"><h1>Дядя Сэм</h1>
             </a>
 
          </div>
+         <div class="tel-fax-mail-head">
+            <a target="_blank" rel="nofollow" href="https://wa.me/77769674545/"><img src="https://img.icons8.com/color/48/000000/whatsapp--v6.png"/></a>
+            <a href="tel:+77769674545">+7 776 967 45 45</a>
+         </div>
+
 
          <div class="panel-control-right"><a href="index.php#" data-activates="slide-out-right" class="sidenav-control-right"><i class="fa fa-shopping-bag"></i>
             <span id="cart-count-span" class="cart-items-count">
@@ -79,23 +81,23 @@
             <ul>
                <p>В корзине: <span class='cart-items-count'>0</span> позиций</p>
                <table class='cart-table row entry'>
-                <tbody class='cart-line-items row entry'>
-                </tbody>
-                <tfoot>
-                 <tr class="cart_footer">
-                  <td colspan='4'>Итого</td>
-                  <td class='cart-subtotal'></td>
-               </tr>
-            </tfoot>
-         </table>
-         <p class='cart-is-empty'>В вашей корзине пусто.</p>
-         <div class="btn_cont">
-            <a class="waves-effect waves-light btn waves-light button" onclick="Cart.empty()">Очистить корзину</a>
-            <a class="waves-effect waves-light btn modal-trigger button" href="#modalzer">Оформить заказ</a>
-            <!-- Modal Trigger -->
-         </div>
-         <!-- end product cart -->
-      </ul>
-      <img class="righ-logo" src="/img/logo.png" alt="">
+                 <tbody class='cart-line-items row entry'>
+                 </tbody>
+                 <tfoot>
+                   <tr class="cart_footer">
+                     <td colspan='4'>Итого</td>
+                     <td class='cart-subtotal'></td>
+                  </tr>
+               </tfoot>
+            </table>
+            <p class='cart-is-empty'>В вашей корзине пусто.</p>
+            <div class="btn_cont">
+               <a class="waves-effect waves-light btn waves-light button" onclick="Cart.empty()">Очистить корзину</a>
+               <a class="waves-effect waves-light btn modal-trigger button" href="#modalzer">Оформить заказ</a>
+               <!-- Modal Trigger -->
+            </div>
+            <!-- end product cart -->
+         </ul>
+         <img class="righ-logo" src="/img/logo.png" alt="">
+      </div>
    </div>
-</div>
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
          <div class="container">
             <h2>При заказе до 16:00 скидка 15%*</h2>
             <p>*Подробности уточняйте у оператора </p>
-           <!--  <a class="button" href="/">Shop Now</a> -->
+            <!--  <a class="button" href="/">Shop Now</a> -->
          </div>
       </div>
    </div>
@@ -17,60 +17,66 @@
       <img class="lazyload" data-src="img/slider3.jpg" alt="">
       <div class="caption">
          <div class="container">
-           <h2>Подпишись на наш инстаграм и получай скидку 10%</h2>
-           <a class="inst" target="_blank" rel="nofollow" href="https://www.instagram.com/ddsam_ukg"><img src="/img/inst.png" alt=""><span>ddsam_ukg</span></a>
+          <h2>Подпишись на наш инстаграм и получай скидку 10%</h2>
+          <a class="inst" target="_blank" rel="nofollow" href="https://www.instagram.com/ddsam_ukg"><img src="/img/inst.png" alt=""><span>ddsam_ukg</span></a>
 <!--             <p>Find your need now</p>
-            <a class="button" href="/">Shop Now</a> -->
-         </div>
-      </div>
-   </div>   
-   <div class="slider-entry">
-<!--       <div class="overlay"></div> -->
-      <img style="object-fit: contain;" class="lazyload" data-src="img/kombo1.jpg" alt="">
-      <div class="caption">
-         <div class="container">
+   <a class="button" href="/">Shop Now</a> -->
+</div>
+</div>
+</div>   
+<div class="slider-entry">
+   <!--       <div class="overlay"></div> -->
+   <img style="object-fit: contain;" class="lazyload vdesc" data-src="img/kombo1.jpg" alt="">
+   <img style="object-fit: contain;" class="lazyload vmob" data-src="img/mkombo1.jpg" alt="">
+   <div class="caption">
+      <div class="container">
          <!--    <h2>НОВИНКА</h2> -->
 <!--             <p>Find your need now</p>
-            <a class="button" href="/">Shop Now</a> -->
-         </div>
-      </div>
-   </div>   <div class="slider-entry">
-<!--       <div class="overlay"></div> -->
-      <img style="object-fit: contain;" class="lazyload" data-src="img/kombo2.jpg" alt="">
-      <div class="caption">
-         <div class="container">
+   <a class="button" href="/">Shop Now</a> -->
+</div>
+</div>
+</div>   <div class="slider-entry">
+   <!--       <div class="overlay"></div> -->
+   <img style="object-fit: contain;" class="lazyload vdesc" data-src="img/kombo2.jpg" alt="">
+   <img style="object-fit: contain;" class="lazyload vmob" data-src="img/mkombo2.jpg" alt="">
+   <div class="caption">
+      <div class="container">
          <!--    <h2>НОВИНКА</h2> -->
 <!--             <p>Find your need now</p>
-            <a class="button" href="/">Shop Now</a> -->
-         </div>
-      </div>
-   </div>   
-   <div class="slider-entry">
-      <div class="overlay"></div>
-      <img class="lazyload" data-src="img/slider3.jpg" alt="">
-      <div class="caption">
-         <div class="container">
-            <h2>Скоро в нашем меню появятся <br> вкуснейшие пиццы от Дяди Сэма</h2>
+   <a class="button" href="/">Shop Now</a> -->
+</div>
+</div>
+</div>   
+<div class="slider-entry">
+   <div class="overlay"></div>
+   <img class="lazyload" data-src="img/slider3.jpg" alt="">
+   <div class="caption">
+      <div class="container">
+         <h2>Скоро в нашем меню появятся <br> вкуснейшие пиццы от Дяди Сэма</h2>
 <!--             <p>Find your need now</p>
-            <a class="button" href="/">Shop Now</a> -->
-         </div>
-      </div>
-   </div>   
-   <div class="slider-entry">
-      <div class="overlay"></div>
-      <img class="lazyload" data-src="img/slider3.jpg" alt="">
-      <div class="caption">
-         <div class="container">
-            <h2>Кейтеринг (выездное обслуживание)</h2>
-            <p>   подготовка меню и расчет стоимости меню
-   тестинг – предварительная дегустация блюд их для оценки заказчиками
-   планирование пространства
-   сценарий кейтеринга  – планирование времени, составление схемы обслужтвания
-   менеджмент кейтеринга – составление транспортного плана, списки персонала, схемы ввоза и вывоза.</p>
-            <!-- <a class="button" href="/">Shop Now</a> -->
-         </div>
+   <a class="button" href="/">Shop Now</a> -->
+</div>
+</div>
+</div>   
+<div class="slider-entry">
+   <div class="overlay"></div>
+   <img class="lazyload" data-src="img/slider3.jpg" alt="">
+   <div class="caption">
+      <div class="container">
+         <h2 style="text-align: left;">Кейтеринг (выездное обслуживание)</h2>
+         <ul style="text-align: left;">
+            <li>- подготовка меню и расчет стоимости меню</li>
+            <li>- тестинг – предварительная дегустация блюд их для оценки заказчиками</li>
+            <li>- планирование пространства</li>
+            <li>- сценарий кейтеринга  – планирование времени</li>
+            <li>- составление схемы обслужтвания</li>
+            <li>- менеджмент кейтеринга</li>
+            <li>– составление транспортного плана, списки персонала, схемы ввоза и вывоза</li>
+         </ul>
+         <!-- <a class="button" href="/">Shop Now</a> -->
       </div>
    </div>
+</div>
 </div>
 <div class="menu app-section appl-section">
    <div class="container">
@@ -91,7 +97,7 @@
             
             <li class="tab"><a href="index.html#t4" class="clickmepls">Напитки</a></li>
             
-          <!--   <li class="tab"><a href="index.html#t6" class="clickmepls">Все меню</a></li> -->
+            <!--   <li class="tab"><a href="index.html#t6" class="clickmepls">Все меню</a></li> -->
          </ul>
          <div id="t1">
             <div class="row">
@@ -115,16 +121,16 @@
                <?php display_products('zakus', $array); ?>
             </div>
          </div>
-      <div id="t4">
-         <div class="row">
-            <?php display_products('drink', $array); ?>
-         </div>
-      </div>      
-      <div id="t5">
-         <div class="row">
-            <?php display_products('fastfood', $array); ?>
-         </div>
-      </div>      
+         <div id="t4">
+            <div class="row">
+               <?php display_products('drink', $array); ?>
+            </div>
+         </div>      
+         <div id="t5">
+            <div class="row">
+               <?php display_products('fastfood', $array); ?>
+            </div>
+         </div>      
  <!--      <div id="t6">
          <div class="row">
             // <?php //display_all($array); ?>
@@ -143,36 +149,36 @@
       <div class="row">
          <div class="col s6">
             <div class="entry">
-              <img class="about_img" src="/img/grill.png" alt="">
-               <h5>ГОТОВИМ НА ГРИЛЕ</h5>
-               <p>В отличии от других способов приготовления гриль не требует использование растительного масла, а это значит вы можете не переживать за своё здоровье!</p>
-            </div>
-         </div>
-         <div class="col s6">
-            <div class="entry">
-               <img class="about_img" src="/img/chef.png" alt="">
-               <h5>УНИКАЛЬНОСТЬ НАШЕЙ КУХНИ.</h5>
-               <p>Наше меню было разработано Шеф - поваром 6 разряда, поэтому вы можете быть уверены в уникальном вкусе!</p>
-            </div>
-         </div>
-      </div>
-      <div class="row">
-         <div class="col s6">
-            <div class="entry">
-               <img class="about_img" src="/img/pack.png" alt="">
-               <h5>УПАКОВКА.</h5>
-               <p>Мы используем продуманную систему упаковки для сохранения первоначального вида и вкуса нашей американской кухни!</p>
-            </div>
-         </div>
-         <div class="col s6">
-            <div class="entry">
-               <img class="about_img" src="/img/pre.png" alt="">
-               <h5>ПРЕДЗАКАЗ.</h5>
-               <p>На нашем сайте вы можете оформить заказ до 16:00 на любое удобное для вас время и получить скидку 15%. Вы экономите свое время и деньги!</p>
-            </div>
+             <img class="about_img" src="/img/grill.png" alt="">
+             <h5>ГОТОВИМ НА ГРИЛЕ</h5>
+             <p>В отличии от других способов приготовления гриль не требует использование растительного масла, а это значит вы можете не переживать за своё здоровье!</p>
+          </div>
+       </div>
+       <div class="col s6">
+         <div class="entry">
+            <img class="about_img" src="/img/chef.png" alt="">
+            <h5>УНИКАЛЬНОСТЬ НАШЕЙ КУХНИ.</h5>
+            <p>Наше меню было разработано Шеф - поваром 6 разряда, поэтому вы можете быть уверены в уникальном вкусе!</p>
          </div>
       </div>
    </div>
+   <div class="row">
+      <div class="col s6">
+         <div class="entry">
+            <img class="about_img" src="/img/pack.png" alt="">
+            <h5>УПАКОВКА.</h5>
+            <p>Мы используем продуманную систему упаковки для сохранения первоначального вида и вкуса нашей американской кухни!</p>
+         </div>
+      </div>
+      <div class="col s6">
+         <div class="entry">
+            <img class="about_img" src="/img/pre.png" alt="">
+            <h5>ПРЕДЗАКАЗ.</h5>
+            <p>На нашем сайте вы можете оформить заказ до 16:00 на любое удобное для вас время и получить скидку 15%. Вы экономите свое время и деньги!</p>
+         </div>
+      </div>
+   </div>
+</div>
 </div>
 <div class="open-hours app-section">
    <div class="container">
